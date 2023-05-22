e1c8bb3b8da81ca831135b6fdfd3597e64dc403b
Anibal-Alpizar <anibal.alpizar14@gmail.com>
2023-05-21 20:19:27 -0600

    transaction form

--- src/components/TransactionForm.jsx
+++ src/components/TransactionForm.jsx
@@ -1,5 +1,35 @@
+import { useState } from "react";
+import { useGlobalState } from "../context/GlobalState";
+
 function TransactionForm() {
-  return <div>TransactionForm</div>;
+  const { addTransaction } = useGlobalState();
+  const [description, setDescription] = useState("");
+  const [amount, setAmount] = useState(0);
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    addTransaction({ description, amount });
+    console.log({ id: 1, description, amount });
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Enter a description"
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <input
+          type="number"
+          step={0.01}
+          placeholder="0.00"
+          onChange={(e) => setAmount(e.target.value)}
+        />
+        <button>Add Transaction</button>
+      </form>
+    </div>
+  );
 }
 
 export default TransactionForm;
