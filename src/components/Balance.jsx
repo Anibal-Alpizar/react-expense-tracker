7dae8394d0ce5f41cbc7c8e95126991ddb099cd3
anibal-alpizar <anibal.alpizar14@gmail.com>
2023-05-27 20:44:23 -0600

    React Victory

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -3,7 +3,7 @@ import { useGlobalState } from "../context/GlobalState";
 function Balance() {
   const { transactions } = useGlobalState();
   const amouts = transactions.map((transaction) => transaction.amount);
-  const total = amouts.reduce((acc, item) => (acc += item), 0);
+  const total = amouts.reduce((acc, item) => (acc += item), 0).toFixed(2);
   return (
     <div className="flex justify-between">
       <h3>Your Balance</h3>
--- src/components/transactions/TransactionForm.jsx
+++ src/components/transactions/TransactionForm.jsx
@@ -13,6 +13,8 @@ function TransactionForm() {
       description,
       amount: +amount,
     });
+    setAmount(0);
+    setDescription("");
   };
 
   return (
@@ -23,6 +25,7 @@ function TransactionForm() {
           placeholder="Enter a description"
           onChange={(e) => setDescription(e.target.value)}
           className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full"
+          value={description}
         />
         <input
           type="number"
@@ -30,6 +33,7 @@ function TransactionForm() {
           placeholder="0.00"
           onChange={(e) => setAmount(e.target.value)}
           className="bg-zinc-600 text-white px-3 py-2 rounded-lg block mb-2 w-full"
+          value={amount}
         />
         <button className="bg-indigo-700 text-white px-3 py-3 rounded-lg block mb-2 w-full">
           Add Transaction
